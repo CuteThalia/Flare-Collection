955e5b5eb3f70be3d56ab7fbab2df90007c79807
AdamKyle <a-kyle@hotmail.com>
2015-10-27 00:10:33 -0600

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,17 @@ var babelify = require("babelify");
 
 
 gulp.task('make:flare-currency', function() {
-    return browserify('src/add-new-currency/flare-currency.js')
-        .transform(babelify)
-        .bundle()
-        .pipe(source('Flare-Currency.js'))
-        .pipe(gulp.dest('dist/flare/currency/'));
+  return browserify('src/add-new-currency/flare-currency.js')
+      .transform(babelify)
+      .bundle()
+      .pipe(source('Flare-Currency.js'))
+      .pipe(gulp.dest('dist/flare/currency/'));
+});
+
+gulp.task('make:npc-speach-box', function() {
+  return browserify('src/npc-speach-box/npc_speach_box.js')
+      .transform(babelify)
+      .bundle()
+      .pipe(source('Flare-NPCSpeachBox.js'))
+      .pipe(gulp.dest('dist/flare/npc-speach-box/'));
 });
